063805140b7a73f2d4a1904e763422b11137ce17
Alejandro Nanez <alejonanez@gmail.com>
2016-03-03 14:30:19 -0500

    Initial Commit

---
+++ Bombs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bombs
+//
+//  Created by Alejo Nanez Ortiz on 3/3/16.
+//  Copyright © 2016 Alejandro Nanez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
